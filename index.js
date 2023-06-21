271a6e052a15740809f74ee44e7b604a8e81b578
Priyanshu Tripathi <priyanshu@clementius.com>
2023-06-21 19:38:14 +0530

    update url

--- index.js
+++ index.js
@@ -2,51 +2,51 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(cors());
-app.use(bodyParser.json({limit: "100mb"}))
+app.use(bodyParser.json({ limit: "100mb" }))
 
 
-const axios=require('axios')
+const axios = require('axios')
 
-app.post('/apiCall', async(req, res) => {
+app.post('/apiCall', async (req, res) => {
 
 
 
-    let data={
-        username : "Chrome.Extension",
-        password : "Chrome@2023!"
-    }
+  let data = {
+    username: "Chrome.Extension",
+    password: "Chrome@2023!"
+  }
 
-    try{
-        let {username,password} = req.body;
+  try {
+    let { username, password, url } = req.body;
 
 
     const token = `${username}:${password}`;
     const encodedToken = Buffer.from(token).toString('base64');
-    const session_url = 'https://app.orcanos.com/orcanosdemo/api/v2/Json/QW_Login';
+    const session_url = `https://app.orcanos.com/${url}/api/v2/Json/QW_Login`;
 
     var config = {
       method: 'post',
       url: session_url,
-      headers: { 'Authorization': 'Basic '+ encodedToken }
+      headers: { 'Authorization': 'Basic ' + encodedToken }
     };
 
-     let response = await axios(config);
-     if(response.status == 200){
-        return res.status(200).send({data:response.data})
-     }
-     else{
-        return res.status(400).send({error:"Wrong Credentials"})
-     }
+    let response = await axios(config);
+    if (response.status == 200) {
+      return res.status(200).send({ data: response.data })
+    }
+    else {
+      return res.status(400).send({ error: "Wrong Credentials" })
+    }
 
 
-//    const token = `username:password`;
-//     const encodedToken = Buffer.from(token).toString('base64');
-//     console.log('encodedToken', encodedToken)
+    //    const token = `username:password`;
+    //     const encodedToken = Buffer.from(token).toString('base64');
+    //     console.log('encodedToken', encodedToken)
 
-}
-  catch(error){
+  }
+  catch (error) {
     console.log(error)
   }
 })
